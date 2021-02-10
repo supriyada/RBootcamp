c6f58884f0b152ba258a6efc7597cdba7826966e
supriyada <supriyada@gmail.com>
2021-02-09 21:58:26 -0500

    Classwork Day 6

---
+++ cw/01-Lesson-Plans/03-Python/1/Activities/02-Stu_HelloVariableWorld/Unsolved/HelloVariableWorld.py
@@ -0,0 +1,32 @@
+# Create a variable called 'name' that holds a string
+name = "Priya"
+
+# Create a variable called 'country' that holds a string
+country = "India"
+
+# Create a variable called 'age' that holds an integer
+age = 33
+
+# Create a variable called 'hourly_wage' that holds an integer
+hourly_wage = 20.0
+
+# Calculate the daily wage for the user
+daily_wage = 8*hourly_wage
+
+# Create a variable called 'satisfied' that holds a boolean
+satisfied = True
+
+# Print out "Hello <name>!"
+print("Hello " + name +"!")
+
+# Print out what country the user entered
+print("The country entered is:"+country)
+
+# Print out the user's age
+print("The age of "+name+"is:"+ str(age))
+
+# With an f-string, print out the daily wage that was calculated
+print(f"The daily wage is:{daily_wage}")
+
+# With an f-string, print out whether the users were satisfied
+print(f"Is {name} satisfied? {satisfied}")
