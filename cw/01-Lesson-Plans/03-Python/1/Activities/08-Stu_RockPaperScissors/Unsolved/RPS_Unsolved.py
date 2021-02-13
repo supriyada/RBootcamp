c12365a5a4e0fe1d9a16f841ba9db188ca84dbc7
supriyada <supriyada@gmail.com>
2021-02-12 22:32:03 -0500

    Classwork Day 7

--- cw/01-Lesson-Plans/03-Python/1/Activities/08-Stu_RockPaperScissors/Unsolved/RPS_Unsolved.py
+++ cw/01-Lesson-Plans/03-Python/1/Activities/08-Stu_RockPaperScissors/Unsolved/RPS_Unsolved.py
@@ -11,6 +11,24 @@ options = ["r", "p", "s"]
 computer_choice = random.choice(options)
 
 # User Selection
-user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ")
+#user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ")
 
 # Run Conditionals
+
+option = 'y'
+while option == 'y':
+    user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ")
+    if((user_choice == "r" and computer_choice == 'r') or (user_choice == "s" and computer_choice == "s") or (user_choice == "p" and computer_choice == "p")):
+        print("It's a smashing tie!!!")
+        print("The user choice is: "+user_choice+" and computer's choice is: "+computer_choice)
+    elif((user_choice == 'r' and computer_choice == 's') or (user_choice == 's' and computer_choice == "p") or (user_choice == 'p' and computer_choice == "r")):
+        print("You won!!!")
+        print("The user choice is: "+user_choice+" and computer's choice is: "+computer_choice)
+    elif((user_choice == 's' and computer_choice == 'r') or (user_choice == 'p' and computer_choice == "s") or (user_choice == 'r' and computer_choice == "p")):
+        print("You Loose!!!")
+        print("The user choice is: "+user_choice+" and computer's choice is: "+computer_choice)
+    else:
+        print("Please enter valid option")
+    option = input("\nDo you want to continue? (y)es or (n)o: ")
+
+
