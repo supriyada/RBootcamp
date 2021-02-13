e9b909361805dc84fb34549f28ff55098f73ba8f
supriyada <supriyada@gmail.com>
2021-02-12 22:32:03 -0500

    Classwork Day 7

--- cw/01-Lesson-Plans/03-Python/1/Activities/04-Stu_DownToInput/Unsolved/DownToInput.py
+++ cw/01-Lesson-Plans/03-Python/1/Activities/04-Stu_DownToInput/Unsolved/DownToInput.py
@@ -1,11 +1,15 @@
 # Take input of you and your neighbor
-
+my_name=input("Please enter your name: ")
+neigh_name = input("Please enter your neighbour's name: ")
 
 # Take how long each of you have been coding
-
+my_exp = int(input("How long have you been coding? "))
+neigh_exp = int(input("How long has you neighbour been coding? "))
 
 # Add total month
-
+total_month = my_exp + neigh_exp
 
 # Print results
+print("Hello "+ my_name)
+print(f"You and your neighbour has been coding for: {str(total_month)}")
 
